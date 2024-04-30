57f63eefe79a158fe9929aaa0b862cee3d4c8845
7pageMUDA <pasricha.nimit@gmail.com>
2024-04-29 21:44:46 -0500

    Add pixelated effect on hovering

--- javascript.js
+++ javascript.js
@@ -3,12 +3,29 @@ const container = document.querySelector(".container");
 function createGrid() {
     for (let i = 0; i < 16; i++) {
         const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
         gridRow.style.display = "flex";
         container.appendChild(gridRow);
         for (let j = 0; j < 16; j++) {
             const gridElement = document.createElement("div");
+            gridElement.classList.add("grid-element");
+            gridElement.style.width = "60px";
+            gridElement.style.height = "60px";
             gridRow.appendChild(gridElement);
         }
     }
 }
 createGrid();
+
+function hoverEffect() {
+    const gridElements = document.querySelectorAll(".grid-element");
+    gridElements.forEach((element) => {
+        element.addEventListener("mouseover", () => {
+            element.style.backgroundColor = "blue";
+        });
+        element.addEventListener("mouseout", () => {
+            element.style.backgroundColor = "white";
+        });
+    })
+}
+hoverEffect();
