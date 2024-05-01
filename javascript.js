8731ac2e66f331701ced14b6f849da80e160cd3c
nimit-pasricha <pasricha.nimit@gmail.com>
2024-04-30 21:06:19 -0500

    Make grid squares change size to fit 960px^2 based on user input for number of squares

--- javascript.js
+++ javascript.js
@@ -1,41 +1,31 @@
 const container = document.querySelector(".container");
 
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-    let numSquaresPerSide;
-    // keep asking user for input until it is in range (0, 100]
-    do {
-        numSquaresPerSide = +prompt("Enter number of squares per side");
-    } while (!(numSquaresPerSide > 0 && numSquaresPerSide <= 100));
-});
-
-function createGrid() {
+function createGrid(numSquaresPerSide = 16) {
     // create 16 rows of flexbox each called gridRow
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < numSquaresPerSide; i++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
         gridRow.style.display = "flex";
         container.appendChild(gridRow);
 
         // create 16 divs in each row
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < numSquaresPerSide; j++) {
             const gridElement = document.createElement("div");
             gridElement.classList.add("grid-element");
-            gridElement.style.width = "60px";
-            gridElement.style.height = "60px";
+            gridElement.style.width = `${960 / numSquaresPerSide}px`;
+            gridElement.style.height = `${960 / numSquaresPerSide}px`;
             gridRow.appendChild(gridElement);
         }
     }
+    hoverEffect();
 }
-
-createGrid();
 
 function hoverEffect() {
     const gridElements = document.querySelectorAll(".grid-element");
     // div turns blue when mouse enters
     gridElements.forEach((element) => {
         element.addEventListener("mouseover", () => {
-            element.style.backgroundColor = "black";
+            element.style.backgroundColor = "red";
         });
         // div turns back to white when mouse leaves
         element.addEventListener("mouseout", () => {
@@ -44,4 +34,19 @@ function hoverEffect() {
     });
 }
 
-hoverEffect();
+createGrid();
+
+// when user clicks on the button,
+// keep asking for input until it is in range (0, 100]
+const button = document.querySelector("button");
+let numSquaresPerSide;
+button.addEventListener("click", () => {
+    do {
+        numSquaresPerSide = +prompt("Enter number of squares per side");
+        console.log("Hello");
+    } while (!(numSquaresPerSide > 0 && numSquaresPerSide <= 100));
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createGrid(numSquaresPerSide);
+});
