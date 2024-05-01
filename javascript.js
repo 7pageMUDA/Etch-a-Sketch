82ad92571a89dffe8213af01eb1e7a07b4d1a648
nimit-pasricha <pasricha.nimit@gmail.com>
2024-04-30 20:16:18 -0500

    Change pixelated color to black

--- javascript.js
+++ javascript.js
@@ -1,11 +1,14 @@
 const container = document.querySelector(".container");
 
 function createGrid() {
+    // create 16 rows of flexbox each called gridRow
     for (let i = 0; i < 16; i++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
         gridRow.style.display = "flex";
         container.appendChild(gridRow);
+
+        // create 16 divs in each row
         for (let j = 0; j < 16; j++) {
             const gridElement = document.createElement("div");
             gridElement.classList.add("grid-element");
@@ -15,17 +18,21 @@ function createGrid() {
         }
     }
 }
+
 createGrid();
 
 function hoverEffect() {
     const gridElements = document.querySelectorAll(".grid-element");
+    // div turns blue when mouse enters
     gridElements.forEach((element) => {
         element.addEventListener("mouseover", () => {
-            element.style.backgroundColor = "blue";
+            element.style.backgroundColor = "black";
         });
+        // div turns back to white when mouse leaves
         element.addEventListener("mouseout", () => {
             element.style.backgroundColor = "white";
         });
-    })
+    });
 }
+
 hoverEffect();
